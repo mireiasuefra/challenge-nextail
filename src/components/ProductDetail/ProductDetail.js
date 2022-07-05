4764ce9f3b775a1cbf9182640b5869d28f880f20
mireiasuefra <mireia.suefra@gmail.com>
2022-07-05 13:55:16 +0200

    add styles to product detail

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,16 +1,34 @@
 import "./ProductDetail.scss";
 
 function ProductDetail(props) {
+  const stockout = props.product.stockout_rate * 100;
+  const isGoodCoverage = props.product.wh_coverage > 0.5;
 
   return (
     <li className="product-detail">
-       <img
-          className="product-detail__image"
-          src={`images/${props.product.code}.jpg`}
-          alt={props.product.name}
-        ></img>
+      <img
+        className="product-detail__image"
+        src={`images/${props.product.code}.jpg`}
+        alt={props.product.name}
+      ></img>
+      <small className="product-detail__code">{props.product.code}</small>
       <h2 className="product-detail__name">{props.product.name}</h2>
-      <p className="product-detail__price"> {props.product.price} €</p>
+      <div>
+        <p className="product-detail__price"> {props.product.price} €</p>
+      </div>
+      {/* <div>GRAFICA</div>*/}
+      <div className="product-detail__stockout">
+        <p className="product-detail__stockout-percent">{stockout.toFixed(1)} % </p>
+        <small className="product-detail__stockout-text">stockout</small>
+      </div>
+      <div className="product-detail__coverage">
+        {isGoodCoverage ? (
+          <p className="product-detail__coverage--good">Good</p>
+        ) : (
+          <p className="product-detail__coverage--low">Very Low</p>
+        )}
+        <p className="product-detail__coverage-text">wh coverage</p>
+      </div>
     </li>
   );
 }
